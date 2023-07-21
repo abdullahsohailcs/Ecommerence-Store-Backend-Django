042ae7e954ac259d311ab255573e93ebf87b13ce
Abdullah-Sohail100 <90067650+Abdullah-Sohail100@users.noreply.github.com>
2023-07-22 00:12:04 +0500

    Custom Filter created for admin

--- store/admin.py
+++ store/admin.py
@@ -1,11 +1,26 @@
-from typing import Any
+from typing import Any, List, Optional, Tuple
 from django.contrib import admin
 from django.db.models.query import QuerySet
 from django.http.request import HttpRequest
 from . import models
 from django.db.models.aggregates import Count,Min,Max,Avg, Sum
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
 
 # Register your models here.
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'iventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+         return [
+              ('<10','Low')
+         ]
+    
+    def queryset(self, request, queryset):
+        if self.value() == '<10':
+             return queryset.filter(inventory__lt=10)
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
@@ -13,6 +28,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable=['unit_price']
     list_per_page=10
     list_select_related = ['collection']
+    list_filter=['collection','last_update',InventoryFilter]
 
     def collection_title(self, product):
         return product.collection.title
@@ -26,10 +42,27 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name','membership']
+    search_fields=['first_name__istartswith','last_name__istartswith']
+    list_display = ['first_name', 'last_name','membership','orders_count']
     list_editable=['membership']
     ordering=['first_name', 'last_name']
     list_per_page=10
+
+    @admin.display(ordering='orders_count')
+    def orders_count(self, customer):
+            url = (
+                reverse('admin:store_order_changelist')
+                + '?'
+                + urlencode({
+                    'collection__id': str(customer.id)
+                 }))
+            return format_html('<a href="{}">{}</a>',url,customer.orders_count)
+
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders_count = Count('order')
+        )
 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
@@ -41,7 +74,14 @@ class CollectionAdmin(admin.ModelAdmin):
     list_display=['title', 'products_count']
     @admin.display(ordering='products_count')
     def products_count(self, collection):
-        return collection.products_count
+            url = (
+                reverse('admin:store_product_changelist')
+                + '?'
+                + urlencode({
+                    'collection__id': str(collection.id)
+                 }))
+            return format_html('<a href="{}">{}</a>',url,collection.products_count)
+
     
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
